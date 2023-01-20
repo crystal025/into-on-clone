59a485555d91ea1ada38dcdeeb64feeb721e38a6
new-crystal <oj3339@naver.com>
2023-01-20 13:32:17 +0900

    recruit page animation

--- src/page/recruit/components/RecruitPeople.jsx
+++ src/page/recruit/components/RecruitPeople.jsx
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 import RecruitPeopleImg from "./RecruitPeopleImg";
+import useScrollFadeIn from "../../../hooks/useScrollFadeIn"
 
 const RecruitPeople = () => {
+  const animatedItem = useScrollFadeIn();
   const imgList = [
     {
       url: "http://into-on.com/wp-content/uploads/2018/11/1_gray.jpg",
@@ -29,10 +31,10 @@ const RecruitPeople = () => {
     },
   ];
   return (
-    <Container>
+    <Container {...animatedItem}>
       {imgList.map((img, i) => {
         return (
-          <RecruitPeopleImg key={i} url={img.url} colorUrl={img.colorUrl} />
+           <RecruitPeopleImg key={i} url={img.url} colorUrl={img.colorUrl} />
         );
       })}
     </Container>
