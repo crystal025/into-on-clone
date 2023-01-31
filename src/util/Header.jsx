5b460f3bdd873c4a0c4f590a179fe8868adbfd9e
new-crystal <oj3339@naver.com>
2023-01-31 09:41:10 +0900

    edit scroll animation react speed

--- src/util/Header.jsx
+++ src/util/Header.jsx
@@ -144,7 +144,7 @@ const Circle = styled.div`
   width: 2rem;
   height: 2rem;
   line-height: 0%;
-  border: 1px solid
+  border: 2px solid
     ${(props) => (props.color === "true" ? "#e71e38" : "#9fa0a0")};
   color: ${(props) => (props.color === "true" ? "#e71e38" : "#9fa0a0")};
   border-radius: 50%;
@@ -162,7 +162,7 @@ const CircleEn = styled.div`
   width: 2rem;
   height: 2rem;
   line-height: 0%;
-  border: 1px solid
+  border: 2px solid
     ${(props) => (props.color === "true" ? "#9fa0a0" : "#e71e38")};
   color: ${(props) => (props.color === "true" ? "#9fa0a0" : "#e71e38")};
   border-radius: 50%;
