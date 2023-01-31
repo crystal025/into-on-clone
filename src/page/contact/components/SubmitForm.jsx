09c59d74fae6dc17d8a0ee23275e0a0b10b4032b
new-crystal <oj3339@naver.com>
2023-01-31 09:32:51 +0900

    add react-hook-form

--- src/page/contact/components/SubmitForm.jsx
+++ src/page/contact/components/SubmitForm.jsx
@@ -1,19 +1,66 @@
 import styled from "styled-components";
+import { useForm } from "react-hook-form";
 
 const SubmitForm = () => {
+  const {
+    register,
+    setError,
+    getValues,
+    formState: { errors, isDirty, isSubmitting },
+    handleSubmit,
+  } = useForm({ criteriaMode: "all", mode: "onChange" });
+
+  const onVaild = async (data) => {
+    console.log(data);
+  };
   return (
-    <FormBox>
+    <FormBox onSubmit={handleSubmit(onVaild)}>
       <FormText>Name</FormText>
-      <FormInput />
+      <FormInput
+        name="name"
+        type="name"
+        aria-invalid={!isDirty ? undefined : errors.name ? "true" : "false"}
+        {...register("name", {
+          required: "필수 입력 필드입니다.",
+        })}
+      />
+      {errors.name !== undefined && <ErrorMsg>{errors.name.message}</ErrorMsg>}
       <FormText>E-mail</FormText>
-      <FormInput />
+      <FormInput
+        name="email"
+        type="email"
+        aria-invalid={!isDirty ? undefined : errors.email ? "true" : "false"}
+        {...register("email", {
+          required: "필수 입력 필드입니다.",
+          pattern: {
+            value:
+              /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/,
+            message: "이메일이 형식에 맞지 않습니다.",
+          },
+        })}
+      />
+      {errors.email !== undefined && (
+        <ErrorMsg>{errors.email.message}</ErrorMsg>
+      )}
       <FormText>Subject</FormText>
       <FormInput />
       <FormText>Message</FormText>
-      <FormTextArea />
+      <FormTextArea
+        name="message"
+        type="message"
+        aria-invalid={!isDirty ? undefined : errors.message ? "true" : "false"}
+        {...register("message", {
+          required: "필수 입력 필드입니다.",
+        })}
+      />
+      {errors.message !== undefined && (
+        <ErrorMsg>{errors.message.message}</ErrorMsg>
+      )}
       <FooterBox>
         <FooterText>+82 2 3275 3031 / sales@into-on.com</FooterText>
-        <SubmitBtn>SUBMIT</SubmitBtn>
+        <SubmitBtn type="submit" disabled={isSubmitting}>
+          SUBMIT
+        </SubmitBtn>
       </FooterBox>
     </FormBox>
   );
@@ -77,5 +124,13 @@ const SubmitBtn = styled.button`
   background: #e71e38;
   border: 0;
   margin-bottom: 50px;
+`;
+
+const ErrorMsg = styled.p`
+  color: #e71e38;
+  font-size: 13px;
+  font-weight: 900;
+  letter-spacing: 0px;
+  font-style: normal;
 `;
 export default SubmitForm;
